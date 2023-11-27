ad31faf89790c0582ad00acb92fd6c0de62a1372
DylanGReinosoT <dgreinoso@espe.edu.ec>
2023-11-27 00:46:17 -0500

    Se subio el proyecto

---
+++ Eventos/Eventos/Models/EventosContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventos.Models;
+
+public partial class EventosContext : DbContext
+{
+    public EventosContext()
+    {
+    }
+
+    public EventosContext(DbContextOptions<EventosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Libro> Libros { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+            //      => optionsBuilder.UseSqlServer("server=localhost\\sqlexpress; database=EVENTOS; integrated security=true; Trusted_Connection=True; TrustServerCertificate=True;");
+
+        }
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Libro>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Libro__3214EC076FA4D815");
+
+            entity.ToTable("Libro");
+
+            entity.Property(e => e.Autor)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Genero)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
